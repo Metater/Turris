a478b7342b9e4b1c2f522877e5500dc4c900d5a7
Connor <61102529+Metater@users.noreply.github.com>
2021-10-02 23:33:02 -0400

    Pathfinding

---
+++ World/Pathfinding/PathfindingNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class PathfindingNode
+{
+    public readonly Vector2Int pos;
+    public readonly bool walkable;
+
+    public int g, h, f;
+    public PathfindingNode parentNode;
+
+    public PathfindingNode(Vector2Int pos, bool walkable)
+    {
+        this.pos = pos;
+        this.walkable = walkable;
+    }
+}
---
+++ World/Pathfinding/WorldPathfinder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class WorldPathfinder
+{
+    private PathfindingNode[,] nodemap = new PathfindingNode[(World.TilemapRadius * 2) + 1, (World.TilemapRadius * 2) + 1];
+
+    public WorldPathfinder(World world, Vector2Int startPos, Vector2Int finishPos)
+    {
+        for (int y = -World.TilemapRadius; y <= World.TilemapRadius; y++)
+        {
+            for (int x = -World.TilemapRadius; x <= World.TilemapRadius; x++)
+            {
+                Vector2Int pos = new Vector2Int(x, y);
+                SetPathfindingNode(pos, new PathfindingNode(pos, world.GetWorldTile(pos).walkable));
+            }
+        }
+    }
+
+    public void SetPathfindingNode(Vector2Int pos, PathfindingNode node)
+    {
+        nodemap[pos.x + World.TilemapRadius, pos.y + World.TilemapRadius] = node;
+    }
+
+    public PathfindingNode GetPathfindingNode(Vector2Int pos)
+    {
+        return nodemap[pos.x + World.TilemapRadius, pos.y + World.TilemapRadius];
+    }
+}
