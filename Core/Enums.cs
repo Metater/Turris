e6159177df9310151bcae0eab3f2b935d379524b
Connor <61102529+Metater@users.noreply.github.com>
2021-10-11 20:13:31 -0400

    e

--- Core/Enums.cs
+++ Core/Enums.cs
@@ -8,41 +8,6 @@
 
 // need a way to say client left to client server
 
-// Events, Spawn, Despawn, GameInfo are reliable-ordered; Snapshots are sequenced
-public enum PacketType : byte
-{
-    // Sent by a client server saying to a client to spawn their player in and weather or not they are the leader
-    // Received by one client
-    GameInfo,
-    // Sent by a client server saying any new entity spawned in
-    // Received by everyone but sender, true clients only
-    SpawnEntity,
-    // Sent by a client server saying any entity despawned
-    // Received by everyone but sender, true clients only
-    DespawnEntity,
-    // Sent by a client server or client
-    // if (client server) Send a snapshot for all non other player entities
-    // else Send a snapshot for themselves, their player only
-    // Received by everyone but sender
-    EntitySnapshot, // IS A TRUE CLIENT INPUT
-    // Sent by a player saying they did something
-    // Received by everyone but sender
-    EntityEventOut, // IS A TRUE CLIENT INPUT
-    // Sent by client server giving the results of something that happened in the game
-    // Received by everyone but sender, true clients only
-    EntityEventIn,
-    // Sent by client server saying something happened with a tile in the game
-    // Received by everyone but sender, true clients only
-    TileEventIn,
-}
-
-public enum PacketRoutingType : byte
-{
-    BroadcastButToSender = 0,
-    SendToClientServer = 1,
-    SendToClient = 2,
-}
-
 // Comes from a client decision
 public enum EntityEventOutType : byte
 {
@@ -69,11 +34,4 @@ public enum WorldTileType : byte
 public enum TileEventInType : byte
 {
 
-}
-
-public enum GameInfoType : byte
-{
-    SelfJoin,
-    Join,
-    Leave
 }
--- Network/Core/PacketData.cs
+++ Network/Core/PacketData.cs
@@ -37,37 +37,4 @@ public static class PacketData
         return (EntityType)bitReader.GetByte();
     }
     #endregion EntityType
-
-    #region PacketType
-    public static void Put(this BitWriter bitWriter, PacketType value)
-    {
-        bitWriter.Put((byte)value);
-    }
-    public static PacketType GetPacketType(this BitReader bitReader)
-    {
-        return (PacketType)bitReader.GetPacketRoutingType();
-    }
-    #endregion PacketType
-
-    #region PacketRoutingType
-    public static void Put(this BitWriter bitWriter, PacketRoutingType value)
-    {
-        bitWriter.Put((byte)value);
-    }
-    public static PacketRoutingType GetPacketRoutingType(this BitReader bitReader)
-    {
-        return (PacketRoutingType)bitReader.GetByte();
-    }
-    #endregion PacketRoutingType
-
-    #region GameInfoType
-    public static void Put(this BitWriter bitWriter, GameInfoType value)
-    {
-        bitWriter.Put((byte)value);
-    }
-    public static GameInfoType GetGameInfoType(this BitReader bitReader)
-    {
-        return (GameInfoType)bitReader.GetByte();
-    }
-    #endregion GameInfoType
 }
